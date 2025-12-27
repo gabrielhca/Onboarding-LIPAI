b2f8407362e2c7292ed0648a035d1f4a8a01e7a0
Gabriel Amorim <gabrielo.hca@gmail.com>
2025-12-26 23:05:20 -0300

    aula 01 - introdução a orientação a objetos

---
+++ python-introducao/src/07-orientacao-a-objetos/aula01.py
@@ -0,0 +1,67 @@
+""" Aula 01 - Introdução à Orientação a Objetos """
+
+# paradigma de programação
+
+# calcular area e perimetro de um retângulo
+# area = base * altura
+# perimetro = 2 * (base + altura)
+# estrutura para armazenar os valores necessários para os calculos
+
+
+# solução usando programação procedural
+retangulo1 = {
+    "base": 10.0,
+    "altura": 5.0
+}
+
+retangulo2 = {
+    "base": 6.0,
+    "altura": 3.0
+}
+
+# realizar os cálculos
+
+
+def calcular_area(retangulo):
+    return retangulo["base"] * retangulo["altura"]
+
+
+def calcular_perimetro(retangulo):
+    return 2 * (retangulo["base"] + retangulo["altura"])
+
+
+print(calcular_area(retangulo1))
+print(calcular_area(retangulo2))
+print(calcular_perimetro(retangulo1))
+print(calcular_perimetro(retangulo2))
+
+# utilizando orientação a objetos
+
+# classe representa um conceito/modelo
+# a classe possui atributos (características): base e altura
+# a classe possui métodos (comportamentos): calcular_area e calcular_perimetro
+
+
+class Retangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+    def calcular_perimetro(self):
+        return 2 * (self.base + self.altura)
+
+
+# intanciando um objeto do tipo Retangulo
+retangulo1 = Retangulo(10.0, 5.0)
+retangulo2 = Retangulo(6.0, 3.0)
+
+print(type(retangulo1), retangulo1)
+print(type(retangulo2), retangulo2)
+
+print(retangulo1.base, retangulo1.altura,
+      retangulo1.calcular_area(), retangulo1.calcular_perimetro())
+print(retangulo2.base, retangulo2.altura,
+      retangulo2.calcular_area(), retangulo2.calcular_perimetro())
